f537adb30c46fd4b6d97781812412d0112bf9183
Efrén Fuentes <efrenfuentes@gmail.com>
2014-10-07 10:26:54 -0430

    Start db2 domain

---
+++ lib/apolo/domains/db2.rb
@@ -0,0 +1,11 @@
+module Apolo
+  module Domains
+    # Get data from DB2 instance
+    #
+    # @author Efren Fuentes <efrenfuentes@gmail.com>
+    class DB2
+      def initialize
+      end
+    end
+  end
+end
